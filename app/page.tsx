e19377783757415fd3bf4f7f04ceb0662e9af3eb
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2024-12-13 16:37:42 +0530

    Update: Added supabase, Signin/Signup

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,16 @@
-'use client'
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
-import Button from "@/components/button";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-
-/**
- * The homepage of the application.
- *
- * Displays a call-to-action to log in to their account using one of their
- * streaming platform accounts.
- *
- * @returns The JSX element representing the homepage.
- */
-
-export default function Home() {
-  const router = useRouter();
-  const handleCLick = () => {
-    return () => {
-      router.push('/dashboard');
-    }
-  }
+export default async function Index() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="bg-gray-900 rounded-lg shadow-lg p-8 w-full max-w-md">
-        <div className="flex items-center justify-center mb-8">
-          <Image src="/Logo-Transparent.png" alt="Game Triggers" width={150} height={30} />
-        </div>
-        <h1 className="text-white text-2xl font-bold mb-4">Welcome back!</h1>
-        <p className="text-gray-400 mb-8">Login to your account by using one of your streaming platform accounts.</p>
-        <div className="space-y-4">
-          <Button providerName={'Twitch'} bgColor={'purple'} method="Login" onClick={handleCLick()}/>
-          <Button providerName={"Youtube"} bgColor={"red"} method="Login" onClick={handleCLick()}/>
-        </div>
-        <p className="text-gray-400 mt-8 text-center">
-          Don&apos;t have an account yet? <Link href="/signup" className="text-purple-600 hover:text-purple-700">Sign up</Link>
-        </p>
-      </div>
-    </div>
+    <>
+      <Hero />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
+    </>
   );
 }
