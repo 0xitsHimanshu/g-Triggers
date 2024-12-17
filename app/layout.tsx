1813db3433b3d457b0f45cc1a15949affd1d010c
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2024-12-17 18:39:47 +0530

    Feature: Added NextAuth
    using nextauth for twitch and its currently working very fine

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
@@ -27,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
