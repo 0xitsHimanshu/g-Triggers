f9a0f234c46e7ae80caca1a518b690cced209a27
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2024-12-23 18:18:48 +0530

    Update: Added Connect & Disconnect account functions
    Co-Authored-By: Himanshu <189447198+0xtheDestineOne@users.noreply.github.com>

--- components/user-Detail.tsx
+++ components/user-Detail.tsx
@@ -1,23 +1,22 @@
-// File: components/UserDetails.tsx
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
 
-export default function UserDetails({ userData}: { userData: Object}) {
-  const [Data, setData] = useState(userData);
-  const { data: session } = useSession();
-  
-  if(session)
-    console.log(session)
+export default function UserDetails({ user }: { user: any }) {
+  const [userData, setUserData] = useState(user);
+
+  useEffect(() => {
+    setUserData(user);
+  }, [user]);
 
   return (
     <div className="flex flex-col gap-2 items-start">
       <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-      <pre className="text-xs font-mono p-3 rounded border max-h-96 overflow-auto">
-        {JSON.stringify(Data, null, 2)}
+      <pre
+        className="text-xs text-wrap font-mono p-3 rounded border w-[780px] max-h-96 overflow-auto"
+      >
+        {JSON.stringify(userData, null, 2)}
       </pre>
-
     </div>
   );
 }
