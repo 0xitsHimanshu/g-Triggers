659ace827789ade1a076ccfba1ef383f7f6dde1b
Himanshu <70974599+0xitsHimanshu@users.noreply.github.com>
2025-01-29 17:46:38 +0530

    Refactor: Remove unused authentication routes and clean up user data retrieval

--- utils/getCurrentUser.ts
+++ utils/getCurrentUser.ts
@@ -3,7 +3,5 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function getCurrentUser() {
   const session = await getServerSession(authOptions);
-  if(session) console.log(session?.user);
-  
   return session?.user || null; // Return user if authenticated, else null
 }
